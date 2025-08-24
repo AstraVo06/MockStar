75c5553de5797559234a9c7e3b5a37eb6a70e948
Tra Vo <trasongvo@gmail.com>
2025-08-24 11:44:17 -0400

    dashboard modify

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { UserAuth} from '../context/AuthContext';
+
 
 const Dashboard = () => {
+const {session, signOut} = UserAuth();
+const navigate = useNavigate();
+console.log(session);
   return (
     <div>Dashboard</div>
   )
